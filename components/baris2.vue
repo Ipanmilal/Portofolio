15fa837b4018f34a0774ae8345b9217d4a061468
Ipanmilal <ipansaepulmilal13@gmail.com>
2024-06-07 12:16:44 +0700

    make page about

--- components/baris2.vue
+++ components/baris2.vue
@@ -1,5 +1,19 @@
 <template>
-    <div class=" flex h-[400px] w-full border-2 justify-center items-center">
-        <h1>Belum ada konten</h1>
+    <div class="bg-black lg:h-[500px] w-full justify-center sm:h-[500px]">
+        <div class="flex justify-center ">
+            <span class=" text-sky-400 font-bold text-2xl">About Me</span>
+        </div>  
+        
+        <div class="lg:flex lg:justify-center lg:items-center lg:gap-16">
+            <div class="sm:flex sm:justify-center">
+                <img src="/assets/images/urang.png" class="h-[300px] w-[200px]" alt="">
+            </div>
+
+            <div class="lg:w-[500px]">
+                <p class="text-gray-400 lg:text-base sm:text-xs sm:px-2">
+                    introduce myself, my name is Ipan Saepul Milal and my nickname is IPAN. I am a student in the information engineering study program at Bandung College of Technology (STTB) and I am currently studying at MyEduSolve in the Patchway UI/UX Designer section. I have an interest in the world of programming and graphic design. Additionally, I have worked on about 10 programming and graphic design projects. In creating a design such as the UI of an application, I can do it in a minimum of 1 week and it also depends on the results of the research itself. Meanwhile, in making logos
+                </p>
+            </div>
+        </div>
     </div>
 </template>
